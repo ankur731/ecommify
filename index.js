5e32dd7e24da7377b6c2aa03b0e654bd165ba565
unknown <ankur731>
2023-04-05 06:43:23 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,231 @@
+require('dotenv').config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({extended:true}));
+
+mongoose.set('strictQuery', false);
+
+mongoose.connect(process.env.MONGO_URI);
+
+
+const productSchema = new mongoose.Schema(
+	{
+		_id: {
+			type: Number,
+		},
+		name: {
+			type: String,
+		},
+		price: {
+			type: Number,
+		},	
+		color: {
+			type: String,
+		},
+		desc: {
+			type: String,
+		},
+		category: {
+			type: String,
+		},
+		images:[{
+            type: String
+        }],
+        id:{
+            type:String
+        }
+	}
+);
+const Product = mongoose.model("Product", productSchema);
+
+
+const cartSchema = new mongoose.Schema(
+	{
+		_id: {
+			type: String,
+		},
+		name: {
+			type: String,
+		},
+        category:{
+            type:String,
+        },
+		price: {
+			type: Number,
+		},	
+		images:[{
+            type: String
+        }],
+	}
+);
+const Cart = mongoose.model("Cart", cartSchema);
+
+
+const userSchema = new mongoose.Schema({
+    _id:{
+        type:Number
+    },
+    firstName:{
+        type:String
+    },
+    lastName:{
+        type:String
+    },
+    address:{
+        type:String
+    },
+    city:{
+        type:String
+    },
+    email:{
+        type:String
+    },
+    phone:{
+        type:Number
+    },
+    password:{
+        type:String
+    },
+    gender:{
+        type:String
+    }
+});
+
+
+const User = mongoose.model("User", userSchema);
+
+
+app.get("/", function(req, res){
+    
+    Product.find({})
+    .then(function(foundProducts){  
+        res.render("home", {Product:foundProducts});
+    })
+    .catch(err=>{
+        console.log(err);
+    })
+})
+app.get("/shop", function(req, res){
+     Product.find({})
+     .then(function(foundProducts){  
+        res.render("shop", {Product:foundProducts});
+    })
+    .catch(err=>{
+        console.log(err);
+    })
+})
+
+app.get("/shop/:id/addToCart", function(req, res){
+    Product.find({"id":req.params.id})
+    .then(function(foundProduct){
+        Cart.findById(req.params.id)
+        .then(function(cartItem){
+             if(cartItem == [] || cartItem == '[]' || cartItem==null){
+                const newItem = new Cart({_id:req.params.id, name:foundProduct[0].name,category:foundProduct[0].category, price:foundProduct[0].price, images:foundProduct[0].images})
+                newItem.save();
+            }
+            
+            res.redirect("/"+foundProduct[0].category+"/"+foundProduct[0].id);
+             
+        })
+        .catch((error)=>{
+            console.log(error);
+        })
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
+})
+app.get("/:id/deleteFromCart", function(req, res){
+    Cart.findOneAndDelete({_id:req.params.id})
+    .then(function(doc){
+        res.redirect("/cart");
+    })
+    .catch(function(err){
+        console.log(err);
+    })
+})
+app.get("/cart", function(req, res){
+    Cart.find({})
+    .then(function(foundItems){
+        res.render("cart", {items:foundItems});
+    })
+})
+
+app.route("/profile/:userId")
+.get( function(req, res){ 
+    User.find({"_id":req.params.userId})
+        .then(function(foundUser){ 
+        res.render("profile", {user:foundUser});
+    })
+    .catch(error => {
+         console.log(error);
+   });
+})
+.post(function(req, res){
+    
+    User.findOneAndUpdate(
+        {"_id":req.params.userId},
+        {
+            firstName:req.body.firstname,
+            lastName:req.body.lastname,
+            phone:req.body.phone,
+            address:req.body.address,
+            city:req.body.city,
+            email:req.body.email,
+            gender:req.body.gender
+        } 
+    )
+    .then(()=>{
+        res.redirect("/profile/"+req.params.userId);
+    })
+    .catch(error=>{
+        console.log(error);
+    });
+  
+})
+
+    
+   
+
+
+app.get("/:cat", function(req, res){
+Product.find({"category":req.params.cat})
+.then(async function(foundProducts){ 
+    var found = await foundProducts; 
+    // var temp = foundProducts[0].category;
+    var temp = req.params.cat;
+    Category = temp[0].toUpperCase()+temp.slice(1).toLowerCase();
+    res.render("category", {products:found, cat:req.params.cat,id:241, category:Category});
+})
+.catch(err=>{
+    console.log(err);
+})
+})
+app.get("/:cat/:id", function(req, res){
+ 
+Product.find({"id":req.params.id})
+.then(function(foundProduct){ 
+    Product.find({"category":req.params.cat})
+    .then(function(foundProducts){
+        res.render("shop", {Product:foundProduct,cat:req.params.cat, Products:foundProducts});
+    })
+})
+.catch(err=>{
+    console.log(err);
+})
+})
+  
+
+
+app.listen(3000, function(req, res){
+    console.log("server is running on port 3000");
+});
---
+++ public/script.js
@@ -0,0 +1,107 @@
+const bar = document.getElementById('bar');
+const nav = document.getElementById('navbar');
+const close = document.getElementById('close');
+
+if(bar){
+    bar.addEventListener('click', ()=>{
+        nav.classList.add('active');
+    })
+}
+if(close){
+    close.addEventListener('click', ()=>{
+        nav.classList.remove('active');
+    })
+}
+
+var taxRate = 0.05;
+var shippingRate = 15.00; 
+var fadeTime = 300;
+
+
+/* Assign actions */
+$('.product-quantity input').change( function() {
+  updateQuantity(this);
+});
+
+$('.product-removal button').click( function() {
+  removeItem(this);
+});
+
+function code(){
+var code = document.getElementById('coupencode').value;
+if(code=='ankurtiwari'){
+  return 10;
+}
+else{
+  return 0;
+}
+}
+
+/* Recalculate cart */
+function recalculateCart()
+{
+  var subtotal = 0;
+  
+  /* Sum up row totals */
+  $('.product').each(function () {
+    subtotal += parseFloat($(this).children('.product-line-price').text());
+  });
+
+}
+function temptotal(){
+ 
+}
+function total(){
+setTimeout(function() { 
+  var itemPrice=document.querySelectorAll('.product-line-price');
+  var sum=0;
+  for(var i=0;i<itemPrice.length;i++)
+  sum = Number(sum) + Number(itemPrice[i].textContent);
+  var tax=(sum*18)/100;
+  var total=0;
+  var discount = (sum*code())/100;
+  total=Number(sum)+Number(tax.toFixed(2))-Number(discount);
+  document.getElementById('cart-subtotal').textContent=sum.toFixed(2);
+  document.getElementById('cart-tax').textContent=tax;
+  document.getElementById('cart-discount').textContent=discount;
+  document.getElementById('cart-total').textContent=total.toFixed(0);
+}, 500);
+}
+
+/* Update quantity */
+function updateQuantity(quantityInput)
+{
+  /* Calculate line price */
+  var productRow = $(quantityInput).parent().parent();
+  var price = parseFloat(productRow.children('.product-price').text());
+  var quantity = $(quantityInput).val();
+  var linePrice = price * quantity;
+  
+  /* Update line price display and recalc cart totals */
+  productRow.children('.product-line-price').each(function () {
+    $(this).fadeOut(fadeTime, function() {
+      $(this).text(linePrice.toFixed(2));
+      recalculateCart();
+      $(this).fadeIn(fadeTime);
+    });
+  });  
+}
+
+
+/* Remove item from cart */
+function removeItem(removeButton)
+{
+  /* Remove row from DOM and recalc cart total */
+  var productRow = $(removeButton).parent().parent();
+  productRow.slideUp(fadeTime, function() {
+    productRow.remove();
+    recalculateCart();
+  });
+} 
+
+
+window.onload = function () {
+  total();
+  document.getElementById("loading").remove()
+//   document.querySelector(".content").removeAttribute("hidden")
+}
